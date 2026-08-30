3d7bbaf9a394e4ea6357a440f3b58a8127ad5e15
Ishan Koirala <ishankoirala@Ishans-MacBook-Air.local>
2026-08-30 07:11:16 +0545

    67

--- bot/index.js
+++ bot/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
+const crypto = require('crypto');
 const express = require('express');
 const {
   Client,
